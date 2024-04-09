5e760e0293a942fb44e1f707d75397c308d27658
demostanis <demostanis@protonmail.com>
2024-04-10 00:36:42 +0200

    ~ | Make shell.nix more silent

--- shell.nix
+++ shell.nix
@@ -16,14 +16,16 @@ mkShell {
 	];
 
 	shellHook = ''
+		export PIP_DISABLE_PIP_VERSION_CHECK=1
+
 		pnpm install --reporter=silent
 		pnpm install -C frontend --reporter=silent
 
-		make venv
+		make venv >/dev/null
 		source venv/bin/activate
 
 		pip3 install -r requirements.txt --quiet
 
-		${if prod then "pnpm -C frontend build" else ""}
+		${if prod then "pnpm -C frontend build --reporter=silent" else ""}
 	'';
 }
