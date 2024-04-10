8ff343d1b718c35a1aef66c2dbea652754c02342
demostanis <demostanis@protonmail.com>
2024-04-10 21:20:06 +0200

    ~ | Prevent a warning when in debug mode

--- backend/bonk/settings.py
+++ backend/bonk/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "../frontend/build/assets/"
-STATICFILES_DIRS = [
-    BASE_DIR / "../frontend/build/",
-]
+if not DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "../frontend/build/",
+    ]
 DJANGO_VITE_ASSETS_PATH = BASE_DIR / "../frontend/build/"
 DJANGO_VITE_MANIFEST_PATH = DJANGO_VITE_ASSETS_PATH / "manifest.json"
 
