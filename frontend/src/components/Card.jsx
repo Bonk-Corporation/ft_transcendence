d23dc42f4cf66709848de8b3deeb4f7e691cb025
DinoMalin <jcario@student.42lehavre.fr>
2024-04-18 13:58:18 +0200

    + | Added play menu without games

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Card({children}) {
   return (
-    <div class="backdrop-blur-lg w-80 h-80 rounded-md rounded-gradient border-gradient">
+    <div class="backdrop-blur-lg w-80 h-80 rounded-md before:rounded-md border-gradient">
       {children}
     </div>
   );
