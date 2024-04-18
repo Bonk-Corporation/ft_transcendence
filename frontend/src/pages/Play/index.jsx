b2cdae8d33f12fc3fb5ad6825535a8707b7549d2
DinoMalin <jcario@student.42lehavre.fr>
2024-04-18 13:58:18 +0200

    + | Add a play menu without games

--- frontend/src/pages/Play/index.jsx
+++ frontend/src/pages/Play/index.jsx
@@ -12,6 +12,7 @@ export function Play() {
 	});
 
 	return (
+<<<<<<< HEAD
 <<<<<<< HEAD:frontend/src/pages/Home/index.jsx
 		<div class="home">
 			{ me ? <h1 class="font-bold">{me.username} le goat</h1>
@@ -21,6 +22,17 @@ export function Play() {
 			{ me ? <h1>{me.username} le goat</h1>
 >>>>>>> 47e8e36 (~ | Modify some paths):frontend/src/pages/Play/index.jsx
 				 : <h1>Please log in!!!!!</h1>}
+=======
+		<div class="flex flex-col h-full items-center justify-self-center py-40">
+			<div class="w-screen max-w-[1000px] h-full flex">
+				<div class="w-1/2 rounded h-full bg-red-500 mx-4 hover:w-3/4 transition-all">
+
+				</div>
+				<div class="w-1/2 rounded h-full bg-blue-500 mx-4 hover:w-3/4 transition-all">
+
+				</div>
+			</div>
+>>>>>>> 9f34f5d (+ | Add a play menu without games)
 		</div>
 	);
 }
