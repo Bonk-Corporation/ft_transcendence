bcc225ae1252d076993825320e4098c4cfaf3ed7
DinoMalin <jcario@student.42lehavre.fr>
2024-04-18 15:08:57 +0200

    ~ | Update button dimensions in the navbar, hover on menu

--- frontend/src/components/ConnectedLogCard.jsx
+++ frontend/src/components/ConnectedLogCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export function ConnectedLogCard() {
   return (
-    <div className="flex">
+    <div className="flex items-center">
       <a href="/login">
-        <button className="font-medium m-2 border-2 border-white rounded-full px-6 py-2 hover:bg-white hover:text-black transition-all ease-in-out">
+        <button className="font-medium mx-2 border-2 border-white rounded-full px-6 py-2 hover:bg-white hover:text-black transition-all ease-in-out">
           Log in
         </button>
       </a>
       <a href="/signin">
-        <button className="font-medium m-2 rounded-full px-6 py-2 bg-white text-black hover:bg-gray-300 transition-all ease-in-out">
+        <button className="font-medium mx-2 border-2 border-white rounded-full px-6 py-2 bg-white text-black hover:bg-gray-300 hover:border-gray-300 transition-all ease-in-out">
           Sign in
         </button>
       </a>
