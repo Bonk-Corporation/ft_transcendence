a7a34b3841aef0f9dac723262c13d668c6c2284b
DinoMalin <jcario@student.42lehavre.fr>
2024-04-18 14:46:50 +0200

    + | Add play logs in user profiles
    well, i think. your commits are so unobvious

--- frontend/src/components/ConnectedLogCard.jsx
+++ frontend/src/components/ConnectedLogCard.jsx
@@ -4,12 +4,12 @@ export function ConnectedLogCard() {
   return (
     <div className="flex">
       <a href="/login">
-        <button className="font-medium m-2 border-2 border-white rounded-full px-4 py-1 hover:bg-white hover:text-black transition-all ease-in-out">
+        <button className="font-medium m-2 border-2 border-white rounded-full px-6 py-2 hover:bg-white hover:text-black transition-all ease-in-out">
           Log in
         </button>
       </a>
       <a href="/signin">
-        <button className="font-medium m-2 rounded-full px-4 py-1 bg-white text-black hover:bg-gray-300 transition-all ease-in-out">
+        <button className="font-medium m-2 rounded-full px-6 py-2 bg-white text-black hover:bg-gray-300 transition-all ease-in-out">
           Sign in
         </button>
       </a>
