23dafaf516a1e9228e69b876d6a0f33f41357225
DinoMalin <jcario@student.42lehavre.fr>
2024-04-19 15:28:17 +0200

    + | log card

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -15,7 +15,7 @@ export function Login() {
 				<h1 className="text-2xl font-semibold">Log in</h1>
 				<form action="" method="POST" className="w-full flex flex-col items-center">
 					<input className="bg-transparent border-white border-2 rounded-lg my-1 w-full px-2 py-1" placeholder="Username" type="text" />
-					<input className="bg-transparent border-white border-2 rounded-lg my-1 w-full px-2 py-1" placeholder="Password" type="text" />
+					<input className="bg-transparent border-white border-2 rounded-lg my-1 w-full px-2 py-1" placeholder="Password" type="password" />
 				</form>
 				<button className="px-6 py-2 bg-white rounded-lg text-black font-medium my-2 hover:bg-gray-300 transition-all ease-in-out">
 					Log in
