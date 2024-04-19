3c6d0a56439c5768368784708bc465624142b2b3
DinoMalin <jcario@student.42lehavre.fr>
2024-04-19 15:28:17 +0200

    + | Add "log card" (whatever that is)

--- frontend/src/components/NotLogCard.jsx
+++ frontend/src/components/NotLogCard.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export function ConnectedLogCard() {
+export function NotLogCard() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center pr-4">
       <a href="/login">
         <button className="font-medium mx-2 border-2 border-white rounded-full px-6 py-2 hover:bg-white hover:text-black transition-all ease-in-out">
           Log in
