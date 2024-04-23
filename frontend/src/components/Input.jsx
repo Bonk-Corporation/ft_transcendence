1d3808c8eadcc07c1046a9fd1a94a1702c1af6fc
DinoMalin <jcario@student.42lehavre.fr>
2024-04-23 16:56:50 +0200

    + | Handling different input types

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function Input(props) {
   return (
-    <input className={`${props.className} bg-transparent border-white border-2 rounded-lg px-2 py-1`} placeholder={props.placeholder} type="text" />
+    <input className={`${props.className} bg-transparent border-white border-2 rounded-lg px-2 py-1`} placeholder={props.placeholder} type={props.type} />
   );
 }
