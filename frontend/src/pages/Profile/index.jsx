73203587f314ae930f623bb8595052193a9a0f7a
DinoMalin <jcario@student.42lehavre.fr>
2024-04-23 20:13:58 +0200

    + | Add friend list to the profile

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { ProfileCard } from '../../components/ProfileCard';
+import { FriendCard } from '../../components/FriendCard';
 
 export function Profile({profile}) {
   return (
-    <div>
+    <div className="flex ">
       <ProfileCard profile={profile} />
+      <div className="ml-4 max-h-96 px-2 overflow-auto backdrop-blur-lg down-gradient">
+        {
+          profile.friends.map((friend) => (
+            <FriendCard profile={friend}></FriendCard>
+          ))
+        }
+      </div>
     </div>
   );
 }
