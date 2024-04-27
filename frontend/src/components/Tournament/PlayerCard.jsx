02398e43ba19fb278b9298ce5e8a48b7a8ff77ca
DinoMalin <jcario@student.42lehavre.fr>
2024-04-27 15:13:01 +0200

    + | Added tournament page

---
+++ frontend/src/components/Tournament/PlayerCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card } from '../utils/Card';
+
+export function PlayerCard({profile, admin}) {
+  return (
+    <Card className='flex items-center p-2 mb-4 w-80'>
+        <div className={`bg-[url(${profile.avatar})] bg-cover bg-center rounded-full border-2 border-white h-16 aspect-square mr-4`}></div>
+        <div>
+            <h1 className="font font-semibold text-lg">{profile.name}</h1>
+            <h1>Level {profile.level}</h1>
+        </div>
+        {admin ? 
+        <div className="bg-white aspect-square rounded-full ml-10 px-2 py-1 flex items-center shadow">
+            <i className="fa-solid fa-crown text-black"></i>
+        </div>
+         : null}
+    </Card>
+  );
+}
