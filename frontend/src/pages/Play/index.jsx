acd00867121d66c726391e6e73d52727ea4e09f3
DinoMalin <jcario@student.42lehavre.fr>
2024-04-28 17:15:36 +0200

    + | Added access to games

--- frontend/src/pages/Play/index.jsx
+++ frontend/src/pages/Play/index.jsx
@@ -11,14 +11,14 @@ export function Play() {
 	});
 
 	return (
-		<div class="flex flex-col h-full items-center justify-self-center">
-			<div class="w-screen max-w-[1000px] h-96 flex">
-				<div class="w-1/2 rounded h-full bg-red-500 mx-4 hover:w-3/4 transition-all">
-
-				</div>
-				<div class="w-1/2 rounded h-full bg-blue-500 mx-4 hover:w-3/4 transition-all">
-
-				</div>
+		<div className="flex flex-col h-full items-center justify-self-center">
+			<div className="w-screen max-w-[1000px] h-96 flex">
+				<a href="/pong" className="w-1/2 flex justify-center py-4 rounded h-full bg-red-500 mx-4 hover:w-3/4 transition-all">
+					Pong
+				</a>
+				<a href="/bonk" className="w-1/2 flex justify-center py-4 rounded h-full bg-blue-500 mx-4 hover:w-3/4 transition-all">
+					Bonk
+				</a>
 
 			</div>
 		</div>
