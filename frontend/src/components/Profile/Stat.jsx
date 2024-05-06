b716a55b9c9740879d33ad1febf65cc547af3f3c
DinoMalin <jcario@student.42lehavre.fr>
2024-05-06 21:49:59 +0000

    + | Using Apex Chart

--- frontend/src/components/Profile/Stat.jsx
+++ frontend/src/components/Profile/Stat.jsx
@@ -1,45 +1,56 @@
 import React, { useEffect, useRef } from 'react';
 import { Card } from '../utils/Card';
-import Chart from 'chart.js/auto';
+// import Chart from 'chart.js/auto';
+import Chart from "react-apexcharts";
 
 
 export function Stat({label, firstLabel, secondLabel, firstData, secondData, firstColor, secondColor}) {
   const stat = useRef(null);
 
-	useEffect(() => {
-		if (stat) {
-			const data = {
-				labels: [firstLabel, secondLabel],
-				datasets: [{
-					label: label,
-					data: [firstData, secondData],
-					backgroundColor: [firstColor, secondColor],
-					hoverOffset: 4,
-					borderWidth: 0,
-					spacing: 3,
-					borderRadius: 42
-				}]
-			};
-		
-			new Chart(
-				stat.current,
-				{
-					type: 'doughnut',
-					data: data,
-					options: {
-						layout: {
-							padding: 5
-						},
-						cutout: 75
-					}
-				}
-				);
+  const data = {
+          
+	series: [firstData * 100 / (firstData + secondData)],
+	options: {
+	  plotOptions: {
+		radialBar: {
+		  hollow: {
+			size: '70%',
+			background: 'white',
+		  },
+		  track: {
+			background: secondColor,
+			strokeWidth: '100%',
+			dropShadow: {
+				enabled: true,
+				top: 4,
+				left: 4,
+				blur: 8,
+				opacity: 0.5
 			}
-		}, []);
+		  },
+		  dataLabels: {
+			name: {
+			  show: true
+			},
+			value: {
+			  offsetY: 6,
+			  fontSize: '16px',
+			  color: '#2d2d2d',
+			}
+		  }
+		}
+	  },
+	  colors: [firstColor],
+	  labels: [firstLabel],
+	  stroke: {
+		  lineCap: "round",
+	  },
+	},
+  };
 
   return (
-    <Card className="flex items-center justify-center h-60 p-4 aspect-square">
-        <canvas ref={stat} ></canvas>
+    <Card className="flex items-center justify-center p-2 aspect-square max-h-60">
+		<Chart options={data.options} series={data.series} type="radialBar" height={200} />
     </Card>
   );
 }
