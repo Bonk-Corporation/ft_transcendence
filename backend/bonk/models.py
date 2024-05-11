1074208a21352fb613afb580fa56f67535d81239
DinoMalin <jcario@student.42lehavre.fr>
2024-05-11 08:53:26 +0000

    + | Added User to models

--- backend/bonk/models.py
+++ backend/bonk/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
 
 class User(AbstractUser):
-    username = models.CharField(max_length=8, unique=True)
-    display_name = models.CharField(max_length=96)
+    name = models.CharField(max_length=96, unique=True)
+    email = models.EmailField()
+    level = models.PositiveIntegerField()
+    level_percentage = models.PositiveIntegerField()
+    avatar = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="friends")
+    name = models.CharField(max_length=96, unique=True)
+    avatar = models.URLField()
+    level = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class GameHistory(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="game_history"
+    )
+    game = models.CharField(max_length=32)
+    score = models.ArrayField(models.PositiveIntegerField(), size=2)
+    win = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.game} - {'Win' if self.win else 'Loss'}"
