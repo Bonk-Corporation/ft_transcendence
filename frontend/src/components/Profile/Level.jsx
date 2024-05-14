57c710a75e9b624af9653d9e0753be5f705720d0
DinoMalin <jcario@student.42lehavre.fr>
2024-05-14 21:24:11 +0000

    ~ | Modified profile

--- frontend/src/components/Profile/Level.jsx
+++ frontend/src/components/Profile/Level.jsx
@@ -5,7 +5,7 @@ export function Level({level, levelPercentage}) {
   return (
     <div className="w-full transition-all flex flex-col items-center">
       Level {level}
-      <div className="w-full h-2 bg-gray-200/20 rounded-full">
+      <div className="w-full h-2 bg-gray-50/20 rounded-full">
         <div className={`w-[${levelPercentage}%] h-full rounded-full bg-gradient-to-r from-[#9312D0] to-[#45096A]`}/>
       </div>
     </div>
