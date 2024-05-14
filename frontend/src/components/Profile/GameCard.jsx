a5026e6798e437aff1c68d2f70e224b6f6629f77
DinoMalin <jcario@student.42lehavre.fr>
2024-05-14 21:44:32 +0000

    ~ | Modified score

--- frontend/src/components/Profile/GameCard.jsx
+++ frontend/src/components/Profile/GameCard.jsx
@@ -5,10 +5,10 @@ import { Card } from '../utils/Card';
 export function GameCard({game}) {
     return (
         <Card color={`${game.win ? "[#00FF00]" : "[#FF0000]"}`} className='mb-3'>
-            <div className="flex p-4 items-center justify-between">
-                <div className="flex items-center">
+            <div className="flex p-4 items-center justify-between w-80">
+                <div className="flex items-center w-full justify-between">
                     <h1 className="text-xl">{game.game}</h1>
-                    <h1 className="pl-52 font-semibold">{game.score[0]} | {game.score[1]}</h1>
+                    <h1 className="bg-white rounded-full font-semibold text-black px-2">{game.score[0]} | {game.score[1]}</h1>
                 </div>
             </div>
         </Card>
