720cf448f148a6b888b9a612ccf86634fe4960c8
DinoMalin <jcario@student.42lehavre.fr>
2024-05-14 21:14:09 +0000

    ~ | Modified friend card

--- frontend/src/components/Profile/FriendCard.jsx
+++ frontend/src/components/Profile/FriendCard.jsx
@@ -4,13 +4,12 @@ import { Card } from '../utils/Card';
 export function FriendCard({profile}) {
   return (
     <Card className='mb-4'>
-        <div className="flex p-4 items-center justify-between">
+        <div className="flex p-3 items-center justify-between overflow-hidden">
             <div className="flex items-center">
-
-                <div className={`bg-[url(${profile.avatar})] border-2 bg-cover bg-center bg-no-repeat rounded-full w-16 aspect-square `} />
-                <div className="ml-4 mr-16 flex flex-col">
-                    <h1 className="font-semibold text-xl">{profile.name}</h1>
-                    <h1 className="my-1">Level {profile.level}</h1>
+                <div className={`bg-[url(${profile.avatar})] border-2 bg-cover bg-center bg-no-repeat rounded-full w-16 aspect-square`} />
+                <div className="ml-4 mr-16 flex flex-col leading-tight w-36 overflow-hidden">
+                    <h1 className="font-semibold text-lg overflow-hidden text-ellipsis">{profile.name}</h1>
+                    <h1>Level {profile.level}</h1>
                 </div>
             </div>
             <button>
