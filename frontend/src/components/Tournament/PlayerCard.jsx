336b03c18c8c6854a657022842eca9efaf116632
DinoMalin <jcario@student.42lehavre.fr>
2024-05-15 16:15:21 +0000

    ~ | Text ellipsis on tournament player card

--- frontend/src/components/Tournament/PlayerCard.jsx
+++ frontend/src/components/Tournament/PlayerCard.jsx
@@ -6,8 +6,8 @@ export function PlayerCard({profile, admin}) {
     <Card className='flex items-center px-4 py-2 mb-4 w-80'>
       <div className="w-full flex items-center">
         <div className={`bg-[url(${profile.avatar})] bg-cover bg-center rounded-full border-2 border-white h-16 aspect-square mr-4`}></div>
-        <div>
-          <h1 className="font font-semibold text-lg w-full">{profile.name}</h1>
+        <div className="overflow-hidden">
+          <h1 className="font font-semibold text-lg w-full overflow-hidden text-ellipsis">{profile.name}</h1>
           <h1 className="w-full">Level {profile.level}</h1>
         </div>
       </div>
