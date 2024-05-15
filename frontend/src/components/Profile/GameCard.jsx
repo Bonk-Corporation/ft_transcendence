9c2136374bd308571416467260ea43805aeb4f37
DinoMalin <jcario@student.42lehavre.fr>
2024-05-15 13:33:52 +0000

    ~ | Overflow hidden on game history

--- frontend/src/components/Profile/GameCard.jsx
+++ frontend/src/components/Profile/GameCard.jsx
@@ -5,7 +5,7 @@ import { Card } from '../utils/Card';
 export function GameCard({game}) {
     return (
         <Card color={`${game.win ? "[#00FF00]" : "[#FF0000]"}`} className='mb-3'>
-            <div className="flex p-4 items-center justify-between w-80">
+            <div className="flex p-4 items-center justify-between w-80 overflow-x-hidden">
                 <div className="flex items-center w-full justify-between">
                     <h1 className="text-xl">{game.game}</h1>
                     <h1 className="bg-white rounded-full font-semibold text-black px-2">{game.score[0]} | {game.score[1]}</h1>
