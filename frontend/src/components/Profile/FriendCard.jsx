ad77c44f93882d0d52dcf5944a5b5c4feac796cd
DinoMalin <jcario@student.42lehavre.fr>
2024-05-16 10:35:31 +0000

    ~ | Reduced friend card size

--- frontend/src/components/Profile/FriendCard.jsx
+++ frontend/src/components/Profile/FriendCard.jsx
@@ -6,7 +6,7 @@ export function FriendCard({profile}) {
     <Card className='mb-4'>
         <div className="flex p-3 items-center justify-between overflow-hidden">
             <div className="flex items-center">
-                <div className={`bg-[url(${profile.avatar})] border-2 bg-cover bg-center bg-no-repeat rounded-full w-16 aspect-square`} />
+                <div className={`bg-[url(${profile.avatar})] border-2 bg-cover bg-center bg-no-repeat rounded-full w-14 aspect-square`} />
                 <div className="ml-4 mr-16 flex flex-col leading-tight w-36 overflow-hidden">
                     <h1 className="font-semibold text-lg overflow-hidden text-ellipsis">{profile.name}</h1>
                     <h1>Level {profile.level}</h1>
