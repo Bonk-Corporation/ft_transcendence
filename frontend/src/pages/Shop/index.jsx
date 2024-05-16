6eaa1677b7c3ea657d3b33008c295d6ec071e9c9
DinoMalin <jcario@student.42lehavre.fr>
2024-05-16 12:31:02 +0000

    ~ | Fixed arrow buttons on shop carrousel

--- frontend/src/pages/Shop/index.jsx
+++ frontend/src/pages/Shop/index.jsx
@@ -30,7 +30,7 @@ export function Shop() {
         <div className="flex items-center">
           <svg onClick={() => {setPage(page > 0 ? page - 1 : page)}} className={`w-6 ${page <= 0 ? "fill-white/40 hover:cursor-auto" : "fill-white/60 cursor-pointer hover:fill-white"}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"/></svg>
           <h1 className="select-none">{page + 1}</h1>
-          <svg onClick={() => {setPage(page + 1 < items.length / factor ? page + 1 : page)}} className={`w-6 ${page + 1 >= items.length / 12 ? "fill-white/40 hover:cursor-auto" : "fill-white/60 cursor-pointer hover:fill-white"}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg>
+          <svg onClick={() => {setPage(page + 1 < items.length / factor ? page + 1 : page)}} className={`w-6 ${page + 1 >= items.length / factor ? "fill-white/40 hover:cursor-auto" : "fill-white/60 cursor-pointer hover:fill-white"}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg>
         </div> : null
       }
     </div>
