34a3d3dde63c07ffb26ebf24b8851a9e75db0008
DinoMalin <jcario@student.42lehavre.fr>
2024-05-16 12:06:31 +0000

    + | Added filter feature to game history

--- frontend/src/components/Profile/GameCard.jsx
+++ frontend/src/components/Profile/GameCard.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Card } from '../utils/Card';
 
 
-export function GameCard({game}) {
+export function GameCard({game, display, setDisplay}) {
     return (
-        <Card className='mb-3'>
-            <div className="flex px-4 py-3 items-center justify-between w-80 overflow-x-hidden">
-                <div className="flex items-center w-full justify-between">
-                    <h1 className="font-semibold">{game.game}</h1>
-                    <h1 className={`rounded-full font-semibold text-black justify-center px-2 border-2 ${game.win ? "bg-[#32CD32]" : "bg-[#FF6961]"}`}> + {game.xp}</h1>
-                    <h1 className={`bg-white rounded-full font-semibold text-black px-2 border-2 border-[${game.win ? "#32CD32" : "#FF6961"}]`}>{game.score[0]} | {game.score[1]}</h1>
+            display == 'all' || display == game.game ?
+            <Card className='mb-3'>
+                <div className="flex px-4 py-3 items-center justify-between w-80 overflow-x-hidden">
+                    <div className="flex items-center w-full justify-between">
+                        <h1 className="font-semibold hover:underline cursor-pointer"
+                            onClick={() => {display == game.game ? setDisplay('all') : setDisplay(game.game)}}>
+                            {game.game}
+                        </h1>
+                        <h1 className={`rounded-full font-semibold text-black justify-center px-2 border-2 ${game.win ? "bg-[#32CD32]" : "bg-[#FF6961]"}`}> + {game.xp}</h1>
+                        <h1 className={`bg-white rounded-full font-semibold text-black px-2 border-2 border-[${game.win ? "#32CD32" : "#FF6961"}]`}>{game.score[0]} | {game.score[1]}</h1>
+                    </div>
                 </div>
-            </div>
-        </Card>
+            </Card> : null
     );
 }
