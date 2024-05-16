9d01d78ae507f95d10425ee3913042b7d89327dc
BolvicBolvicovic <victor.bolheme@gmail.com>
2024-05-16 13:33:59 +0000

    ~ | Changed vite config, don't work

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import path from "path";
+import wasm from "vite-plugin-wasm-pack";
 
 const resolve = (f) => path.resolve(__dirname, f);
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), wasm("./pong-client")],
   base: "/static/",
   build: {
     manifest: true,
