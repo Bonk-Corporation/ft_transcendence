5d64fe11626d4ced12c9f9a05bfd90d7eb57c06a
DinoMalin <jcario@student.42lehavre.fr>
2024-05-17 16:14:48 +0000

    + | Added xp

--- frontend/src/components/Profile/GameCard.jsx
+++ frontend/src/components/Profile/GameCard.jsx
@@ -12,7 +12,9 @@ export function GameCard({game, display, setDisplay}) {
                             onClick={() => {display == game.game ? setDisplay('all') : setDisplay(game.game)}}>
                             {game.game}
                         </h1>
-                        <h1 className={`rounded-full font-semibold text-black justify-center px-2 border-2 ${game.win ? "bg-[#32CD32]" : "bg-[#FF6961]"}`}> + {game.xp}</h1>
+                        <h1 className={`rounded-full font-semibold text-black justify-center px-2 border-2 flex items-center ${game.win ? "bg-[#32CD32]" : "bg-[#FF6961]"}`}>
+                            + {game.xp}
+                            <p className="font- text-sm">xp</p></h1>
                         <h1 className={`bg-white rounded-full font-semibold text-black px-2 border-2 border-[${game.win ? "#32CD32" : "#FF6961"}]`}>{game.score[0]} | {game.score[1]}</h1>
                     </div>
                 </div>
