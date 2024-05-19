bf4b3de557787e172abfc1058a8ee83e89558fce
DinoMalin <jcario@student.42lehavre.fr>
2024-05-19 22:10:29 +0000

    ~ | Fixed padding bug on tournament menu

--- frontend/src/pages/Tournament/Menu.jsx
+++ frontend/src/pages/Tournament/Menu.jsx
@@ -18,21 +18,57 @@ export function Menu() {
       },
       "nb_players": 8
     },
+    {
+      "author": {
+        "username": "Bolvic",
+        "avatar": "https://user-images.githubusercontent.com/8974888/231858967-7c37bf1e-335b-4f5a-9760-da97be9f54bb.png",
+      },
+      "nb_players": 8
+    },
+    {
+      "author": {
+        "username": "Bolvic",
+        "avatar": "https://user-images.githubusercontent.com/8974888/231858967-7c37bf1e-335b-4f5a-9760-da97be9f54bb.png",
+      },
+      "nb_players": 8
+    },
+    {
+      "author": {
+        "username": "Bolvic",
+        "avatar": "https://user-images.githubusercontent.com/8974888/231858967-7c37bf1e-335b-4f5a-9760-da97be9f54bb.png",
+      },
+      "nb_players": 8
+    },
+    {
+      "author": {
+        "username": "Bolvic",
+        "avatar": "https://user-images.githubusercontent.com/8974888/231858967-7c37bf1e-335b-4f5a-9760-da97be9f54bb.png",
+      },
+      "nb_players": 8
+    },
+    {
+      "author": {
+        "username": "Bolvic",
+        "avatar": "https://user-images.githubusercontent.com/8974888/231858967-7c37bf1e-335b-4f5a-9760-da97be9f54bb.png",
+      },
+      "nb_players": 8
+    },
+    
   ]
   return (
-    <div className="flex items-center h-96">
-      <div className="h-full">
-        <h1 className="px-2 text-lg font-semibold">Create a room</h1>
-        <CTA className="w-72 hover:w-96 h-full">
-          <a href="/tournament/room">
-            <i className="text-6xl fa-solid fa-plus mb-2"></i>
-            <h1 className="text-lg text-center font-semibold">Create a room</h1>
-          </a>
-        </CTA>
+    <div className="flex items-center">
+      <div className="flex flex-col items-center mr-4">
+        <h1 className="text-lg font-semibold">Create a room</h1>
+        <a href="/tournament/room" className="w-full h-full">
+          <CTA className="w-72 hover:w-96 h-96 px-2">
+              <i className="text-6xl fa-solid fa-plus mb-2"></i>
+              <h1 className="text-lg text-center font-semibold">Create a room</h1>
+          </CTA>
+        </a>
       </div>
-      <div className="h-full">
+      <div className="flex flex-col items-center">
         <h1 className="px-2 text-lg font-semibold">Join a room</h1>
-        <div className="overflow-auto overflow-x-hidden px-2 down-gradient">
+        <div className="h-96 backdrop-blur-lg overflow-y-auto overflow-x-hidden pr-2 down-gradient">
           {
             invitations.map((invitation) => (
               <InvitationCard invitation={invitation} />
