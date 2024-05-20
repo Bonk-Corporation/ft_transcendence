61173c66ba9536b43743d78d23e67f2d5729b474
DinoMalin <jcario@student.42lehavre.fr>
2024-05-20 13:01:50 +0000

    ~ | Disabled username on profile config

--- frontend/src/components/utils/Input.jsx
+++ frontend/src/components/utils/Input.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 
 export function Input(props) {
   return (
-    <input className={`${props.className} bg-transparent border-white border-2 rounded-lg px-2 py-1`} placeholder={props.placeholder} type={props.type} />
+    <input {...(props.disabled ? {disabled:true} : null )} className={`${props.className} ${props.disabled ? "cursor-not-allowed" : "cursor-text"} bg-transparent border-white border-2 rounded-lg px-2 py-1`} placeholder={props.placeholder} type={props.type} />
   );
 }
