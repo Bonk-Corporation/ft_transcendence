df08bcfd642b1dadcd38f0b7de96d18deee7eef0
DinoMalin <jcario@student.42lehavre.fr>
2024-05-20 23:54:45 +0000

    ~ | Clearing input on popup closing

--- frontend/src/components/utils/PopUp.jsx
+++ frontend/src/components/utils/PopUp.jsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react';
 import { Card } from './Card';
 
 
-export function PopUp({ active, setActive, children, className = "" }) {
+export function PopUp({ active, setActive, children, className = "", clear = () => {} }) {
+  function handleClick() {
+    setActive(false);
+    clear();
+  }
   return (
     <div className={`${active ? null : "hidden"} w-screen h-screen absolute bg-black/60 top-0 left-0 z-50 flex justify-center items-center`}>
       <Card className='px-3 py-3 flex flex-col items-end'>
-        <i onClick={() => {setActive(false)}} className="fa-solid fa-xmark cursor-pointer"></i>
+        <i onClick={handleClick} className="fa-solid fa-xmark cursor-pointer"></i>
         <div className={`p-6 ${className}`}>
 					{children}
 				</div>
