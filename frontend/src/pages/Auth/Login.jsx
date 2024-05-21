a06c9531440c91d1bebe48b4fce23572790c6725
DinoMalin <jcario@student.42lehavre.fr>
2024-05-21 13:08:44 +0000

    ~ | Changed email to something you want to say

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -32,6 +32,9 @@ export function Login(props) {
 				<CTA onClick={() => props.setTriedLog(true)} className="my-2">Log in</CTA>
 				<a onClick={() => props.setTriedLog(true)} className="underline" href={url}>Log in with 42</a>
 			</div>
+			<div>
+
+			</div>
 			<div className="mt-6 flex flex-col items-center">
 				<p className="text-sm mb-1">No account yet ?</p>
 				<a href="/signup"><CTA>Sign up</CTA></a>
