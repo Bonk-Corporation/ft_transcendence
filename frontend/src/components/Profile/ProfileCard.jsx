d61556841e03845e8c61db283c4aaaf9a657cd38
DinoMalin <jcario@student.42lehavre.fr>
2024-05-21 15:23:18 +0000

    + | Added pic upload on avatar

--- frontend/src/components/Profile/ProfileCard.jsx
+++ frontend/src/components/Profile/ProfileCard.jsx
@@ -5,24 +5,33 @@ import { CTA } from '../utils/CTA';
 import { Level } from './Level';
 
 
-export function ProfileCard({profile}) {
+export function ProfileCard({ profile }) {
   return (
     <>
-        <Card className='flex flex-col items-center justify-center p-6 h-full'>
-            <div className="flex items-center">
-                <div className={`rounded-full md:block hidden w-32 mr-4 aspect-square border-4 bg-[url(${profile ? profile.avatar : null})] bg-center bg-cover`} />
-                <div className="flex flex-col">
-									<Input disabled={true} className="my-2" placeholder={`${profile ? profile.name : null}`}/>
-									<Input className="my-2" placeholder={`${profile ? "Something you want to say..." : null}`} />
-								</div>
+      <Card className='flex flex-col items-center justify-center p-6 h-full'>
+        <div className="flex items-center">
+          <div className={`relative rounded-full md:block hidden w-32 mr-4 aspect-square border-4 bg-[url(${profile ? profile.avatar : null})] bg-center bg-cover`}>
+            <div className="absolute inset-0 z-50 hover:opacity-60 opacity-0 w-full h-full bg-black rounded-full transition-all flex items-center justify-center">
+              <input
+                className="absolute inset-0 w-full h-full opacity-0 cursor-pointer rounded-full"
+                type="file"
+                accept="image/*, jpg, png"
+              />
+              <i className="fa-solid fa-camera text-white text-lg"></i>
             </div>
-						<div className="my-4 flex flex-col w-full">
-								<Input className="w-full mb-2" placeholder="New password"/>
-								<Input className="w-full mt-2" placeholder="Confirm password"/>
-						</div>
-						<CTA className='mb-4'>Update</CTA>
-						<Level level={profile ? profile.level : null} levelPercentage={profile ? profile.levelPercentage : null}/>
-        </Card>
+          </div>
+          <div className="flex flex-col">
+            <Input disabled={true} className="my-2" placeholder={`${profile ? profile.name : ''}`} />
+            <Input className="my-2" placeholder={`${profile ? "Something you want to say..." : ''}`} />
+          </div>
+        </div>
+        <div className="my-4 flex flex-col w-full">
+          <Input className="w-full mb-2" placeholder="New password" />
+          <Input className="w-full mt-2" placeholder="Confirm password" />
+        </div>
+        <CTA className='mb-4'>Update</CTA>
+        <Level level={profile ? profile.level : null} levelPercentage={profile ? profile.levelPercentage : null} />
+      </Card>
     </>
   );
 }
