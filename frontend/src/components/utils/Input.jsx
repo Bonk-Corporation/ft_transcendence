4eb83d3374510131df4ffb3ba79fd558d672dd82
DinoMalin <jcario@student.42lehavre.fr>
2024-05-24 11:45:13 +0000

    + | Max length to something to say

--- frontend/src/components/utils/Input.jsx
+++ frontend/src/components/utils/Input.jsx
@@ -1,8 +1,7 @@
 import React, { forwardRef } from 'react';
 
 export const Input = forwardRef((props, ref) => {
-  return (<input {...(props.disabled ? {disabled:true} : null )}
-    className={`${props.className} ${props.disabled ? "cursor-not-allowed" : "cursor-text"} bg-transparent border-white border-2 rounded-lg px-2 py-1`}
-    placeholder={props.placeholder} type={props.type}
+  return (<input {...props} className={`${props.className} ${props.disabled ? "cursor-not-allowed" : "cursor-text"} bg-transparent border-white border-2 rounded-lg px-2 py-1`}
+
     ref={ref} />)
 });
