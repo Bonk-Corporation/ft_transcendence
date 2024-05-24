491d91a68462db9e7150efd2089c26b8eaa1cea7
DinoMalin <jcario@student.42lehavre.fr>
2024-05-24 11:11:27 +0000

    + | Xp and win auto calculated, xp added on user

--- backend/ponk/private.py
+++ backend/ponk/private.py
@@ -14,12 +14,31 @@ def game_stats(request, *args, **kwargs):
     try:
         data = json.loads(request.body)
 
+        score_user = data["score"][0]
+        score_opponent = data["score"][1]
+
+        win = score_user > score_opponent
+        multiplier = 10 if win else 1
+
+        if score_user == 4 and score_opponent == 1:
+            multiplier = 17.25
+
+        try:
+            xp = (
+                max(score_user, score_opponent) / min(score_user, score_opponent)
+            ) * multiplier
+        except ZeroDivisionError:
+            xp = (max(score_user, score_opponent) / 1) * multiplier + 5
+
+        user = User.objects.get(username=data["user"])
+        user.level_percentage += xp
+        if user.level_percentage >= 100:
+            user.level += user.level_percentage // 100
+            user.level_percentage = user.level_percentage % 100
+        user.save()
+
         GameHistory(
-            user=User.objects.get(username=data["user"]),
-            game=data["game"],
-            score=data["score"],
-            win=data["win"],
-            xp=data["xp"],
+            user=user, game=data["game"], score=data["score"], win=win, xp=xp
         ).save()
 
         return JsonResponse(
