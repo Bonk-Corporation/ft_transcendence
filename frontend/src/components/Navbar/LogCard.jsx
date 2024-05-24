09066f31c95ebc1d937189e4c5752aa3915a98bc
DinoMalin <jcario@student.42lehavre.fr>
2024-05-24 13:48:44 +0000

    ~ | Navbar responsiveness

--- frontend/src/components/Navbar/LogCard.jsx
+++ frontend/src/components/Navbar/LogCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export function LogCard(props) {
   return (
-      <a href="/profile" className="hidden md:flex items-center rounded-full hover:bg-white transition-all group hover:cursor-pointer pr-4">
+      <a href="/profile" className="flex items-center rounded-full hover:bg-white transition-all group hover:cursor-pointer pr-4">
         <div className={`h-12 aspect-square mr-4 rounded-full border-2 border-white bg-[url(${props.user ? props.user.avatar : null})] bg-center bg-cover`}></div>
         <div>
           <h1 className="group-hover:text-black transition-all">{props.user ? props.user.name : null}</h1>
