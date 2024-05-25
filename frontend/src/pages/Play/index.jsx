1335f3efa7b53105664d6640ff27acd224c33031
DinoMalin <jcario@student.42lehavre.fr>
2024-05-25 16:15:19 +0000

    + | Fetching citations in frontend

--- frontend/src/pages/Play/index.jsx
+++ frontend/src/pages/Play/index.jsx
@@ -4,14 +4,14 @@ export function Play() {
 	const [citation, setCitation] = useState(null);
 
 	useEffect(() => {
-		setCitation({
-			"message": "Lorem Ipsum Dolor Sit Amet",
-			"author" : "DinoMalin"
-		})
+		fetch("/api/citation").then(res => res.json().then(data => {
+			if (!data.error)
+				setCitation(data)
+		}));
 	}, [])
 	return (
 		<div className="flex flex-col h-full items-center justify-self-center">
-			{citation ? <h1 className="text-lg mb-4">❝{citation.message}❞ - {citation.author}</h1> : null}
+			{citation ? <h1 className="text-lg mb-4 text-center max-w-[1000px]">❝{citation.citation}❞ - {citation.username}</h1> : null}
 			<div className="w-screen max-w-[1000px] h-96 flex">
 				<a href="/pong" className="w-1/2 flex justify-center py-4 rounded h-full bg-red-500 mx-4 hover:w-3/4 transition-all">
 					Pong
