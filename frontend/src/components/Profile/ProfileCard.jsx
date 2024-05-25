6218bd40f57c16153ef0c4450947446713472cfa
DinoMalin <jcario@student.42lehavre.fr>
2024-05-25 16:15:19 +0000

    + | Fetching citations in frontend

--- frontend/src/components/Profile/ProfileCard.jsx
+++ frontend/src/components/Profile/ProfileCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Card } from '../utils/Card';
 import { Input } from '../utils/Input';
 import { CTA } from '../utils/CTA';
@@ -6,17 +6,28 @@ import { Level } from './Level';
 
 
 export function ProfileCard({ profile, setProfile, setTriedLog }) {
+  const citation = useRef(null);
 
   function logOut() {
     setProfile(null);
     setTriedLog(false);
-	fetch("/auth/logout", {
-	method: "POST",
-	}).then(res => res.json()
-	.then(data => {
-		if (data.success)
-			window.location.pathname = "/login";
-	}));
+    fetch("/auth/logout", {
+      method: "POST",
+    }).then(res => res.json().then(data => {
+      if (data.success)
+        window.location.pathname = "/login";
+    }));
+  }
+
+  function updateProfile() {
+    if (!citation.current.value.trim().length)
+      return;
+    fetch("/api/update_profile", {
+			method: "POST",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify({"citation": citation.current.value}),
+		})
+    citation.current.value = ""
   }
 
   return (
@@ -35,7 +46,7 @@ export function ProfileCard({ profile, setProfile, setTriedLog }) {
           </div>
           <div className="flex flex-col">
             <Input disabled={true} className="my-2" placeholder={`${profile ? profile.name : ''}`} />
-            <Input className="my-2" placeholder={`${profile ? "Something you want to say..." : ''}`} maxlength="256" />
+            <Input ref={citation} className="my-2" placeholder={`${profile ? "Something you want to say..." : ''}`} maxlength="256" />
           </div>
         </div>
         <div className="my-4 flex flex-col w-full">
@@ -43,7 +54,7 @@ export function ProfileCard({ profile, setProfile, setTriedLog }) {
           <Input className="w-full mt-2" placeholder="Confirm password" />
         </div>
         <div className="flex mb-4">
-          <CTA className='mr-4'>Update</CTA>
+          <CTA onClick={updateProfile} className='mr-4'>Update</CTA>
           <CTA onClick={logOut} className='ml-4 border-red-500 border-2 text-red-500 hover:bg-red-500 hover:text-white' transparent={true}>Log out</CTA>
         </div>
         <Level level={profile ? profile.level : null} levelPercentage={profile ? profile.levelPercentage : null} />
