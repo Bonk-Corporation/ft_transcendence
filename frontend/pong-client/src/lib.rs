fceba2c9e2ead1cf644ce5118a4d17ab57d0ca00
kodokaii <kodokai.featheur@gmail.com>
2024-05-25 17:15:46 +0000

    ~ | Fix slide beug

--- frontend/pong-client/src/lib.rs
+++ frontend/pong-client/src/lib.rs
@@ -184,7 +184,7 @@ pub fn start() -> Result<(), JsValue> {
                     game_id,
                     movement: "UP".to_string()
                 };
-                match cloned_ws.send_with_str(("MOVE".to_owned() + &serde_json::to_string(&movement).unwrap()).as_str()) {
+                match cloned_ws.send_with_str(("SMOVE".to_owned() + &serde_json::to_string(&movement).unwrap()).as_str()) {
                     Ok(_) => console_log!("up sent"),
                     Err(err) => console_log!("up failed: {:?}", err)
                 }
@@ -196,7 +196,7 @@ pub fn start() -> Result<(), JsValue> {
                     game_id,
                     movement: "DOWN".to_string()
                 };
-                match cloned_ws.send_with_str(("MOVE".to_owned() + &serde_json::to_string(&movement).unwrap()).as_str()) {
+                match cloned_ws.send_with_str(("SMOVE".to_owned() + &serde_json::to_string(&movement).unwrap()).as_str()) {
                     Ok(_) => console_log!("down sent"),
                     Err(err) => console_log!("down failed: {:?}", err)
                 }
