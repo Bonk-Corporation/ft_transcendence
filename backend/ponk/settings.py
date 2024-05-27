b71be1092c125ec89b1486d3a5dd76fbce465a2c
kodokaii <kodokai.featheur@gmail.com>
2024-05-27 09:59:37 +0000

    ~ | Remove common password validation

--- backend/ponk/settings.py
+++ backend/ponk/settings.py
@@ -148,9 +148,6 @@ AUTH_PASSWORD_VALIDATORS = [
         },
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
