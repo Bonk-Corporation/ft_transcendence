e5013d23b15a3bf1a13b2dbbbfab62db6c5c8072
demostanis <demostanis@protonmail.com>
2024-05-31 01:01:52 +0200

    ~ | Move shell.nix's crap to separate files in startup.d

--- shell.nix
+++ shell.nix
@@ -28,118 +28,26 @@ mkShell {
 
 		tmux
 
+		# rust
 		(with fenix; with latest; combine [
 			minimal.toolchain
 			targets.wasm32-unknown-unknown.latest.rust-std
 		])
-
 		wasm-pack
 	];
 
 	shellHook = ''
-		if ! grep -q PRIVATE_API_TOKEN .env; then
-			echo PRIVATE_API_TOKEN=`openssl rand 16 | base64` >> .env
-		fi
-
-		set -o allexport
-		source .env
-		set +o allexport
-
-		export SHELL=zsh
-		export PS1="[1mBonk Corporation[0m %% "
-		export "PATH=venv/bin/:$PATH"
-		export PIP_DISABLE_PIP_VERSION_CHECK=1
-		export NIX_IGNORE_SYMLINK_STORE=1
-		export NIX_ENFORCE_PURITY=0
-		export PGDATA="backend/.pg"
-		export NIX_IGNORE_SYMLINK_STORE=1
-		export NIX_ENFORCE_PURITY=0
-		export TERM=xterm
-
-		initdb 2>/dev/null
-		pg_ctl -o "-k /tmp" -l backend/pg.log start
-		createdb -h /tmp "$DB_NAME" 2>/dev/null
-		psql -h /tmp -c "
-			CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';
-			GRANT ALL ON DATABASE $DB_NAME TO $DB_USER;
-			ALTER DATABASE $DB_NAME OWNER TO $DB_USER;" \
-			"$DB_NAME"
-
-		yes | pnpm install --reporter=silent
-		yes | pnpm install -C frontend --reporter=silent
-
-		make venv >/dev/null
-		${if prod then
-			''
-				sed -i '1s|.*|#!/app/venv/bin/python3|' venv/bin/pip3
-			''
-		  else
-			''
-				sed -i "1s|.*|#!$PWD/venv/bin/python3|" venv/bin/pip3
-			''
-		}
-		source venv/bin/activate
-
-		pip3 install -r requirements.txt --break-system-packages --quiet
-
-		if [ ! -e .initial_migration_done ]; then
-			python3 backend/manage.py migrate
-			touch .initial_migration_done
-		fi
-
-		export ${if prod then
-			"port=8443"
-		  else
-			"port=8000"
-		        }
-		export ${if prod then
-			"srv_root=/srv/static" # hardcoded in docker
-		  else
-			"srv_root=$PWD/.static"
-		        }
-
-		yes yes | python backend/manage.py collectstatic
-
-		caddy start --adapter caddyfile --config <( cat <<-EOF
-			http://localhost:$port
-
-			handle_path /static/* {
-				root * $srv_root
-				file_server
-			}
-
-			handle {
-				reverse_proxy localhost:8001
-			}
-		EOF
-		)
-		trap "caddy stop" EXIT
-
-		${if prod then
-			''
-				sed -i '/FT_DEBUG/d' .env
-				echo FT_DEBUG=n >> .env
-
-				pnpm -C frontend build
-			''
-		  else
-			''
-				# install git commit hooks
-				pnpx husky install >/dev/null 2>&1
-
-				sed -i '/FT_DEBUG/d' .env
-				echo FT_DEBUG=y >> .env
-
-				tmux set-option -ga ' CLIENT_ID CLIENT_SECRET VITE_STRIPE_API_KEY STRIPE_WEBHOOK_KEY DB_NAME DB_PASS DB_USER FT_DEBUG'
-				tmux new-session -d 'trap : INT; make || $SHELL'
-				tmux split-window -vb 'trap : INT; $SHELL'
-				tmux set -g mouse on # neat
-				tmux split-window -h 'trap : INT; make fdev || $SHELL'
-				tmux split-window -v -c backend/pong-server 'trap : INT; cargo run -r || $SHELL'
-				tmux attach
-			''
-		 }
-		pg_ctl stop
-		exit
+		for script in startup.d/*; do
+			echo sourcing $script...
+			${if prod then
+				''
+				source "$script" true
+				''
+			else
+				''
+				source "$script" false
+				''
+			 }
+		done
 	'';
 }
