6d70f48270bdc2fbfcdf68d35dbed7b00d3b7ded
DropNod <prigentgwechen@gmail.com>
2024-06-02 15:23:45 +0000

    + | Bonk client integration and optimization

--- backend/ponk/settings.py
+++ backend/ponk/settings.py
@@ -56,12 +56,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "ponk.urls"
