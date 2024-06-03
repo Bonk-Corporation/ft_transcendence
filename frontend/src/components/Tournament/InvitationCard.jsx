70aef5f70dd3b9625a878c77c3ac41bd4b2aab60
DinoMalin <jcario@student.42lehavre.fr>
2024-06-03 13:12:05 +0000

    + | Linked front with back on tournaments

--- frontend/src/components/Tournament/InvitationCard.jsx
+++ frontend/src/components/Tournament/InvitationCard.jsx
@@ -1,19 +1,37 @@
 import React, { useContext } from 'react';
 import { Card } from '../utils/Card';
 import { language } from '../../scripts/languages';
-import { LangContext } from '../../Contexts';
 
-export function InvitationCard({ invitation }) {
+import { LangContext } from '../../Contexts';
+import { useLocation } from 'preact-iso';
+
+export function InvitationCard({room, setError }) {
+  const location = useLocation();
 	const lang = useContext(LangContext);
+
+  function joinTournament() {
+    fetch(`/api/tournament/join_room/${room.host_name}`, {
+      method: "POST",
+    }).then(res => {
+      if (!res.ok) {
+        return res.json().then(errData => {
+          throw new Error(errData.error)})
+      }
+      location.route('/tournament/room');
+    }).catch(err => {
+      setError(err.message);
+    })
+  }
+
   return (
       <Card className='py-2 px-4 mb-2 w-72 overflow-hidden hover:from-white/40 hover:to-white/10 cursor-pointer'>
-          <a href="/tournament/room" className="flex items-center">
-            <div className={`bg-[url(${invitation.author.avatar})] bg-center bg-cover w-8 h-8 rounded-full mr-2`}/>
+          <div onClick={joinTournament} href="/tournament/room" className="flex items-center">
+            <div className={`bg-[url(${room.host_avatar})] bg-center bg-cover w-8 h-8 rounded-full mr-2`}/>
             <div className="w-full overflow-hidden">
-                <h1 className="text-lg overflow-hidden text-ellipsis whitespace-nowrap">{invitation.author.username}'s game</h1>
-                <h1 className="text-sm">{invitation.nb_players} {language.players[lang]}</h1>
+                <h1 className="text-lg overflow-hidden text-ellipsis whitespace-nowrap">{room.room_name}</h1>
+                <h1 className="text-sm">{room.players_number} {room.players_number > 1 ? language.players[lang] : language.player[lang].toLowerCase()}</h1>
             </div>
-          </a>
+          </div>
       </Card>
   );
 }
