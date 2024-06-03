0c2fed63a33cf2dd6bf881b6e2498e56ad4fbb7e
Steam Deck User <a.casamitjanalegay@gmail.com>
2024-06-03 18:28:37 +0000

    + | Add and optimize several tournament endpoints

--- backend/ponk/tournament.py
+++ backend/ponk/tournament.py
@@ -31,11 +31,8 @@ def new(request, *args, **kwargs):
     if name == "":
         return JsonResponse({"error": "Room name cannot be empty"}, status=409)
 
-    if size != 2 and size != 4 and size != 8:
+    if size not in [2, 4, 8]:
         return JsonResponse({"error": "Room size can only be 2, 4 or 8"}, status=409)
-
-    if request.user in tournaments:
-        tournaments.pop(requests.user)
 
     tournaments[request.user] = Tournament(
         name=name,
@@ -50,8 +47,24 @@ def new(request, *args, **kwargs):
 
 
 @authenticated
+def set_to_pong(request, *args, **kwargs):
+    if not tournaments.get(request.user):
+        return JsonResponse({"error": "You're not the tournament host"}, status=409)
+    tournaments[request.user].selected_game = "pong"
+    return JsonResponse({"success": True})
+
+
+@authenticated
+def set_to_bonk(request, *args, **kwargs):
+    if not tournaments.get(request.user):
+        return JsonResponse({"error": "You're not the tournament host"}, status=409)
+    tournaments[request.user].selected_game = "bonk"
+    return JsonResponse({"success": True})
+
+
+@authenticated
 def set_to_private(request, *args, **kwargs):
-    if request.user not in tournaments[request.user].host_user:
+    if not tournaments.get(request.user):
         return JsonResponse({"error": "You're not the tournament host"}, status=409)
     tournaments[request.user].private = True
     return JsonResponse({"success": True})
@@ -59,7 +72,7 @@ def set_to_private(request, *args, **kwargs):
 
 @authenticated
 def set_to_public(request, *args, **kwargs):
-    if request.user not in tournaments[request.user].host_user:
+    if not tournaments.get(request.user):
         return JsonResponse({"error": "You're not the tournament host"}, status=409)
     tournaments[request.user].private = False
     return JsonResponse({"success": True})
@@ -80,11 +93,13 @@ def join_room(request, *args, **kwargs):
             {"error": "Room does not exist".format(username)}, status=404
         )
 
-    if tournaments[target_room_host].private == True:
-        if request.user not in target_room_host.friends:
-            return JsonResponse(
-                {"error": "This room is private".format(username)}, status=409
-            )
+    if (
+        tournaments[target_room_host].private
+        and request.user not in target_room_host.friends
+    ):
+        return JsonResponse(
+            {"error": "This room is private".format(username)}, status=409
+        )
 
     if (
         len(tournaments[target_room_host].users)
@@ -103,6 +118,15 @@ def leave_room(request, *args, **kwargs):
     if request.user.current_room == "":
         return JsonResponse({"error": "You're not part of a tournament"}, status=400)
 
+    if request.user.current_room == request.user.username:
+        if len(tournaments[request.user.current_room].users != 1):
+            tournaments[request.user].host_user = User.objects.get(
+                username=tournaments[request.user.current_room].users[1]
+            )
+        else:
+            tournaments.pop(request.user)
+            return JsonResponse({"success": True})
+
     tournaments[User.objects.get(username=request.user.current_room)].users.remove(
         request.user
     )
@@ -112,15 +136,7 @@ def leave_room(request, *args, **kwargs):
 
 @authenticated
 def kick_user(request, *args, **kwargs):
-    if request.user.current_room == "":
-        return JsonResponse({"error": "You're not part of a tournament"}, status=400)
-
-    if (
-        request.user
-        not in tournaments[
-            User.objects.get(username=request.user.current_room)
-        ].host_user
-    ):
+    if not tournaments.get(request.user):
         return JsonResponse({"error": "You're not the tournament host"}, status=409)
 
     username = args[1].get("target")
@@ -143,25 +159,28 @@ def kick_user(request, *args, **kwargs):
 
 @authenticated
 def status(request, *args, **kwargs):
-    if request.user in tournaments:
-        tournament = tournaments[request.user]
-        return JsonResponse(
-            {
-                "name": tournament.name,
-                "host": tournament.host_user.username,
-                "users": [
-                    {
-                        "username": user.username,
-                        "level": user.level,
-                    }
-                    for user in tournament.users
-                ],
-                "selected_game": tournament.selected_game,
-                "size": tournament.room_size,
-                "private": tournament.private,
-            }
-        )
-    return JsonResponse({"error": "You're not part of a tournament"}, status=400)
+    try:
+        room = User.objects.get(username=request.user.current_room)
+    except ObjectDoesNotExist:
+        return JsonResponse({"error": "You're not part of a tournament"}, status=400)
+
+    tournament = tournaments[room]
+    return JsonResponse(
+        {
+            "name": tournament.name,
+            "host": tournament.host_user.username,
+            "users": [
+                {
+                    "username": user.username,
+                    "level": user.level,
+                }
+                for user in tournament.users
+            ],
+            "selected_game": tournament.selected_game,
+            "size": tournament.room_size,
+            "private": tournament.private,
+        }
+    )
 
 
 @authenticated
@@ -191,9 +210,33 @@ def get_all_tournaments_info(request, *args, **kwargs):
     return JsonResponse({"data": tournaments_info})
 
 
+def play(request, *args, **kwargs):
+    if not tournaments.get(request.user):
+        return JsonResponse({"error": "You're not the tournament host"}, status=409)
+
+    users = tournaments[request.user].users
+
+    if len(users) not in [2, 4, 8]:
+        return JsonResponse(
+            {"error": "You must be 2, 4 or 8 players to start a game"}, status=409
+        )
+
+    if tournaments[request.user].selected_game == "pong":
+        games = []
+        for i in range(0, len(users), 2):
+            games.append([users[i].username, users[i + 1].username])
+        return JsonResponse({"games": games})
+
+    # TODO handle if the selected_game is bonk
+
+
+# TODO handle when a game end and handle when a tournament end
+
 urls = [
     path("status", status),  # get informations about current turnament
     path("get_all_tournaments", get_all_tournaments_info),
+    path("set_to_pong", set_to_pong),
+    path("set_to_bonk", set_to_bonk),
     path("set_to_public", set_to_public),
     path("set_to_private", set_to_private),
     path("join_room/<str:host>", join_room),
