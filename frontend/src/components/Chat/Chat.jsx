7cb2be6522abf76592da235a32c31759b1b434e5
DinoMalin <jcario@student.42lehavre.fr>
2024-06-04 12:13:52 +0000

    + | Added avatar to tournament schema

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -1,144 +1,156 @@
-import { useState, useEffect, useContext } from 'preact/hooks';
-import { Message } from './Message';
-import { language } from '../../scripts/languages';
-import { createRef } from 'preact';
-import { ProfileContext, LangContext } from '../../Contexts';
+import { useState, useEffect, useContext } from "preact/hooks";
+import { Message } from "./Message";
+import { language } from "../../scripts/languages";
+import { createRef } from "preact";
+import { ProfileContext, LangContext } from "../../Contexts";
 
 function RoomButton({ withSeparator, children, activeRoom, ...rest }) {
-	return (
-		<>
-			{ withSeparator &&
-				<button class="inline">
-					|
-				</button> }
-			<div class="m-1 inline">
-				<button class={`${activeRoom == children
-					? "bg-gradient-to-br from-black/30 to-black/15"
-					: "bg-gradient-to-br from-black/10 to-black/5"}
-					m-1 p-1 rounded transition`} {...rest}>
-					{ children }
-				</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      {withSeparator && <button class="inline">|</button>}
+      <div class="m-1 inline">
+        <button
+          class={`${
+            activeRoom == children
+              ? "bg-gradient-to-br from-black/30 to-black/15"
+              : "bg-gradient-to-br from-black/10 to-black/5"
+          }
+					m-1 p-1 rounded transition`}
+          {...rest}
+        >
+          {children}
+        </button>
+      </div>
+    </>
+  );
 }
 
 let ws;
 
 export function Chat() {
-	const profile = useContext(ProfileContext);
-	const lang = useContext(LangContext);
+  const profile = useContext(ProfileContext);
+  const lang = useContext(LangContext);
 
-	if (!profile) return;
+  if (!profile) return;
 
-	const ref = createRef();
+  const ref = createRef();
 
-	const me = profile.name;
-	const [hover, setHover] = useState(false);
-	const [shouldShowChat, setShouldShowChat] = useState(false);
-	const [activeRoom, setActiveRoom] = useState("Lobby");
-	const [friends, setFriends] = useState([]);
-	const [messages, setMessages] = useState({});
+  const me = profile.name;
+  const [hover, setHover] = useState(false);
+  const [shouldShowChat, setShouldShowChat] = useState(false);
+  const [activeRoom, setActiveRoom] = useState("Lobby");
+  const [friends, setFriends] = useState([]);
+  const [messages, setMessages] = useState({});
 
-	function sendChat(e) {
-		e.preventDefault();
+  function sendChat(e) {
+    e.preventDefault();
 
-		const input = e.target.querySelector("input");
+    const input = e.target.querySelector("input");
 
-		if (ws) {
-			const users = [me, activeRoom].sort();
-			let actualRoomName = "Lobby";
-			if (activeRoom != actualRoomName)
-				actualRoomName = `${users[0]}_${users[1]}`;
-			if (input.value.trim() == "") return;
+    if (ws) {
+      const users = [me, activeRoom].sort();
+      let actualRoomName = "Lobby";
+      if (activeRoom != actualRoomName)
+        actualRoomName = `${users[0]}_${users[1]}`;
+      if (input.value.trim() == "") return;
 
-			ws.send(JSON.stringify({
-				room: actualRoomName,
-				content: input.value,
-			}));
-			(messages[activeRoom] ??= [])
-				.push(["local", {"content": input.value}]);
-			setMessages({...messages});
-			input.value = "";
-		}
-	}
+      ws.send(
+        JSON.stringify({
+          room: actualRoomName,
+          content: input.value,
+        }),
+      );
+      (messages[activeRoom] ??= []).push(["local", { content: input.value }]);
+      setMessages({ ...messages });
+      input.value = "";
+    }
+  }
 
-	useEffect(() => {
-		ref?.current?.scrollTo(0, 100000);
-	}, [messages, hover]);
+  useEffect(() => {
+    ref?.current?.scrollTo(0, 100000);
+  }, [messages, hover]);
 
-	useEffect(() => {
-		const url = (location.protocol == "https:" ? "wss:" : "ws:")
-			+ "//" + location.host + "/chat";
-		ws = new WebSocket(url);
+  useEffect(() => {
+    const url =
+      (location.protocol == "https:" ? "wss:" : "ws:") +
+      "//" +
+      location.host +
+      "/chat";
+    ws = new WebSocket(url);
 
-		ws.onopen = () =>
-			setShouldShowChat(true);
+    ws.onopen = () => setShouldShowChat(true);
 
-		ws.onmessage = ({ data }) => {
-			const message = JSON.parse(data);
-			switch (message.type) {
-				case "friends":
-					setFriends(message.friends);
-					break;
-				case "message":
-					(messages[message.room] ??= [])
-						.push(["distant", message]);
-					setMessages(messages);
-					break;
-			}
-		}
-		console.log(messages);
-	}, []);
+    ws.onmessage = ({ data }) => {
+      const message = JSON.parse(data);
+      switch (message.type) {
+        case "friends":
+          setFriends(message.friends);
+          break;
+        case "message":
+          (messages[message.room] ??= []).push(["distant", message]);
+          setMessages(messages);
+          break;
+      }
+    };
+  }, []);
 
-	const roomOpts = {
-		activeRoom,
-		onClick: e => setActiveRoom(e.target.textContent)
-	};
+  const roomOpts = {
+    activeRoom,
+    onClick: (e) => setActiveRoom(e.target.textContent),
+  };
 
-	return (
-		// Can't use hover: since it doesn't always trigger
-		// in the same conditions as onmouseenter
-		<div className={`
+  return (
+    // Can't use hover: since it doesn't always trigger
+    // in the same conditions as onmouseenter
+    <div
+      className={`
 			fixed
-			${hover ?
-				"bottom-0 right-0 h-[300px] w-[300px] rounded" :
-				"bottom-[-2rem] right-[-2rem] w-32 h-32 rounded-full backdrop-blur-lg"}
+			${
+        hover
+          ? "bottom-0 right-0 h-[300px] w-[300px] rounded"
+          : "bottom-[-2rem] right-[-2rem] w-32 h-32 rounded-full backdrop-blur-lg"
+      }
 			bg-gradient-to-br from-white/20 to-white/5
 			transition-all
 		`}
-			onMouseEnter={() => setHover(true)}
-			onMouseLeave={() => setHover(false)}
-		>
-			{
-				hover ? <>
-					<div
-						class="m-2 bg-gradient-to-br from-white/30 to-white/15 rounded h-[95%] backdrop-blur"
-					>
-					<div class="overflow-x-scroll max-h-1/5 whitespace-nowrap">
-						<RoomButton {...roomOpts}>Lobby</RoomButton>
-						{friends.map(friend =>
-							<RoomButton withSeparator {...roomOpts}>
-								{ friend }
-							</RoomButton>
-						)}
-					</div>
-					<hr class="border-black/30" />
-					<div ref={ref} class="flex flex-col h-[70%] w-full overflow-y-scroll mb-[4px]">
-						<div class="flex flex-col h-[90%]">
-							{ messages[activeRoom]?.map(([ where, message ]) =>
-								<Message where={where}>{message}</Message>
-							) }
-						</div>
-					</div>
-					<form action="" onSubmit={sendChat}>
-						<input placeholder={language.your_message[lang]} class="self-end w-full p-2 outline-none ml-0 relative b-[5px] rounded-b" />
-					</form>
-				</div>
-				</> : <div class="flex h-full items-center justify-center">
-					<p>Chat</p>
-				</div>
-			}
-		</div>
-	);
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {hover ? (
+        <>
+          <div class="m-2 bg-gradient-to-br from-white/30 to-white/15 rounded h-[95%] backdrop-blur">
+            <div class="overflow-x-scroll max-h-1/5 whitespace-nowrap">
+              <RoomButton {...roomOpts}>Lobby</RoomButton>
+              {friends.map((friend) => (
+                <RoomButton withSeparator {...roomOpts}>
+                  {friend}
+                </RoomButton>
+              ))}
+            </div>
+            <hr class="border-black/30" />
+            <div
+              ref={ref}
+              class="flex flex-col h-[70%] w-full overflow-y-scroll mb-[4px]"
+            >
+              <div class="flex flex-col h-[90%]">
+                {messages[activeRoom]?.map(([where, message]) => (
+                  <Message where={where}>{message}</Message>
+                ))}
+              </div>
+            </div>
+            <form action="" onSubmit={sendChat}>
+              <input
+                placeholder={language.your_message[lang]}
+                class="self-end w-full p-2 outline-none ml-0 relative b-[5px] rounded-b"
+              />
+            </form>
+          </div>
+        </>
+      ) : (
+        <div class="flex h-full items-center justify-center">
+          <p>Chat</p>
+        </div>
+      )}
+    </div>
+  );
 }
--- frontend/src/components/Tournament/Schema.jsx
+++ frontend/src/components/Tournament/Schema.jsx
@@ -3,28 +3,142 @@ import React from "react";
 export function Schema(props) {
   return (
     <div className="h-full w-full flex flex-col justify-center">
-      {props.room.size >= 2 ? (
-        <div className="h-36 flex justify-center">
-          <div className="rounded-t border-4 border-b-0 w-96"></div>
+      {(props.room.size == 8 && props.room.phases.length == 4) ||
+      (props.room.size == 4 && props.room.phases.length == 3) ||
+      (props.room.size == 2 && props.room.phases.length == 2) ? (
+        <div className="flex flex-col items-center">
+          <i className="fa-solid fa-crown pb-1"></i>
+          <div className="h-full flex justify-center">
+            <div
+              className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[3][0].avatar})]`}
+            ></div>
+          </div>
         </div>
+      ) : null}
+      {props.room.size >= 2 ? (
+        <>
+          <div className="h-36 flex justify-center">
+            <div className="rounded-t border-4 border-b-0 w-96"></div>
+          </div>
+          {(props.room.size == 8 && props.room.phases.length >= 3) ||
+          (props.room.size == 4 && props.room.phases.length >= 2) ||
+          (props.room.size == 2 && props.room.phases.length >= 1) ? (
+            <div className="flex justify-center">
+              <div className="w-48 h-full flex justify-center pl-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[2][0].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-48 h-full flex justify-center"></div>
+              <div className="w-48 h-full flex justify-center pr-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[2][1].avatar})]`}
+                ></div>
+              </div>
+            </div>
+          ) : null}
+        </>
       ) : null}
       {props.room.size >= 4 ? (
-        <div className="h-36 flex justify-center">
-          <div className="rounded-t border-4 border-b-0 w-48"></div>
-          <div className="rounded-t w-48"></div>
-          <div className="rounded-t border-4 border-b-0 w-48"></div>
-        </div>
+        <>
+          <div className="h-36 flex justify-center">
+            <div className="rounded-t border-4 border-b-0 w-48"></div>
+            <div className="rounded-t w-48"></div>
+            <div className="rounded-t border-4 border-b-0 w-48"></div>
+          </div>
+          {(props.room.size == 8 && props.room.phases.length >= 2) ||
+          (props.room.size == 4 && props.room.phases.length >= 1) ? (
+            <div className="flex justify-center">
+              <div className="w-24 h-full flex justify-center pl-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[1][0].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-24 h-full flex justify-center"></div>
+              <div className="w-24 h-full flex justify-center pr-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[1][1].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-24 h-full flex justify-center"></div>
+              <div className="w-24 h-full flex justify-center pl-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[1][2].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-24 h-full flex justify-center"></div>
+              <div className="w-24 h-full flex justify-center pr-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[1][3].avatar})]`}
+                ></div>
+              </div>
+            </div>
+          ) : null}
+        </>
       ) : null}
       {props.room.size == 8 ? (
-        <div className="h-36 flex justify-center">
-          <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
-          <div className="rounded-t w-24 h-full"></div>
-          <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
-          <div className="rounded-t w-24 h-full"></div>
-          <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
-          <div className="rounded-t w-24 h-full"></div>
-          <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
-        </div>
+        <>
+          <div className="h-36 flex justify-center">
+            <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
+            <div className="rounded-t w-24 h-full"></div>
+            <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
+            <div className="rounded-t w-24 h-full"></div>
+            <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
+            <div className="rounded-t w-24 h-full"></div>
+            <div className="rounded-t border-4 border-b-0 w-24 h-full"></div>
+          </div>
+          {props.room.phases.length >= 1 ? (
+            <div className="flex justify-center">
+              <div className="w-12 h-full flex justify-center pl-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][0].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-12 h-full flex justify-center"></div>
+              <div className="w-12 h-full flex justify-center pr-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][1].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-12 h-full flex justify-center"></div>
+              <div className="w-12 h-full flex justify-center pl-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][2].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-12 h-full flex justify-center"></div>
+              <div className="w-12 h-full flex justify-center pr-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][3].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-12 h-full flex justify-center"></div>
+              <div className="w-12 h-full flex justify-center pl-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][4].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-12 h-full flex justify-center"></div>
+              <div className="w-12 h-full flex justify-center pr-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][5].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-12 h-full flex justify-center"></div>
+              <div className="w-12 h-full flex justify-center pl-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][6].avatar})]`}
+                ></div>
+              </div>
+              <div className="w-12 h-full flex justify-center"></div>
+              <div className="w-12 h-full flex justify-center pr-1">
+                <div
+                  className={`h-8 aspect-square rounded-full border-4 bg-center bg-cover bg-[url(${props.room.phases[0][7].avatar})]`}
+                ></div>
+              </div>
+            </div>
+          ) : null}
+        </>
       ) : null}
     </div>
   );
